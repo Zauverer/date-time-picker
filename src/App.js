81dc4fe1c441a9435cc5b0622bfd3bfca5391067
Cristian Ignacio Gomez-Loaiza Videla <cristian.gomez@uss.cl>
2022-09-28 17:40:04 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import setHours from "date-fns/setHours";
+import setMinutes from "date-fns/setMinutes";
+import dateFormat, { masks } from "dateformat";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+/*     const [startDate, setStartDate] = useState(new Date());
+    return (
+      <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+    ); */
+
+    const [startDate, setStartDate] = useState(
+      setHours(setMinutes(new Date(), 30), 17)
+    );
+
+    
+    return (
+      
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => {const xx = setStartDate(date)
+          //timespamp
+          //const dateString = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss")
+          const dateString = dateFormat(new Date(date), "yyyy-mm-dd HH:MM:ss")
+          console.log(dateString)
+        }
+        
+        }
+        showTimeSelect
+        minTime={setHours(setMinutes(new Date(), 30), 17)}
+        maxTime={setHours(setMinutes(new Date(), 30), 20)}
+        dateFormat="MMMM d, yyyy h:mm aa"
+        timeIntervals={60}
+/*        onChange={(date) => {
+          const dateString = new Date(date).toLocaleDateString()
+          console.log(dateString)
+          const Currentdate=dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss")
+          console.log(Currentdate) }*/
+        
+        
+      />
+    );
+  
 }
 
 export default App;
